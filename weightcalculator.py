3fb566ea4391502657a306d6b4e6be2803dc8cf0
James Babwah <jamesbabwah@nc.edu.tt>
2022-09-23 21:55:32 -0400

    added comments to show what needs to be done

--- weightcalculator.py
+++ weightcalculator.py
@@ -1,5 +1,11 @@
-
-
-
 class WeightCalculator:
-    ...
+    #Functions that accept weight on earth and return the weight to given planets
+
+
+#Allows code to be run only if "weightcalculator.py" is ran
+#This part won't be executed if the file is imported as a module
+if __name__ == '__main__':
+    #Input weight
+    #Input planet to calculate weight for
+    #Calculate weight
+    #Print weight
