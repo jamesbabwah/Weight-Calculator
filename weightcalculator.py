a8e45bf74d5bedd0d7c8186c1c6dba719452ba57
James Babwah <jamesbabwah@nc.edu.tt>
2022-09-24 15:23:06 -0400

    added contributors

--- weightcalculator.py
+++ weightcalculator.py
@@ -1,5 +1,6 @@
 class WeightCalculator:
     #Functions that accept weight on earth and return the weight to given planets
+    ...
 
 
 #Allows code to be run only if "weightcalculator.py" is ran
@@ -9,3 +10,4 @@ if __name__ == '__main__':
     #Input planet to calculate weight for
     #Calculate weight
     #Print weight
+    ...
